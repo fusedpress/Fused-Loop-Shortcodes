8e3bf7d5ed8c63e0dea36c26d1d4e6245f64bda6
Anu Sharma <anu@cosmiccoders.com>
2013-02-27 17:48:19 +0530

    Add

---
+++ fp-loop-shortcodes.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * PLugin Name: Fused Loop Shortcodes
+ * Author: Anu Sharma
+ * Plugin URI:
+ * Author URI:http://buddydev.com/members/anusharma/
+ * Version:1.0
+ * Description:  The Shortcode Library for FusedPress Loop Shortcodes.
+ */
+
+?>
