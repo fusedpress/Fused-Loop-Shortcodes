3bbfc7669bd50d86a7754235199b7ffbf5f581df
Anu Sharma <anu@cosmiccoders.com>
2013-02-27 18:35:32 +0530

    1st commit

--- fp-loop-shortcodes.php
+++ fp-loop-shortcodes.php
@@ -7,5 +7,45 @@
  * Version:1.0
  * Description:  The Shortcode Library for FusedPress Loop Shortcodes.
  */
+define('FP_LOOP_SHORTCODES_DIR_PATH', plugin_dir_path(__FILE__));
 
+class FPLoopShortCodes {
+    
+    private static $instance;
+
+    function __construct() {
+  
+        //register shortcodes
+        $this->register_shortcodes();
+  
+    }
+   
+    /**
+     * Register  shortcodes
+     * 
+     */
+    private function register_shortcodes() {
+
+       
+        add_shortcode('date', array($this, 'get_date'));
+
+    }
+    /**
+     * Get Instance
+     * 
+     * Use singlten patteren
+     * @return type
+     */
+    public static function get_instance() {
+
+        if (!isset(self::$instance))
+            self::$instance = new self();
+
+        return self::$instance;
+    }
+    public function get_date($atts){
+        return date('F jS, Y');
+    }
+}
+FPLoopShortCodes::get_instance();
 ?>
