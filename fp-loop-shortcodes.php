0e8dafe00bec10972a281686b4dda452ebdccc66
Anu Sharma <anu@cosmiccoders.com>
2013-02-28 16:59:01 +0530

    2nd commit

--- fp-loop-shortcodes.php
+++ fp-loop-shortcodes.php
@@ -26,8 +26,24 @@ class FPLoopShortCodes {
      */
     private function register_shortcodes() {
 
-       
+       //Date 
         add_shortcode('date', array($this, 'get_date'));
+        add_shortcode('modified-date',array($this,'modified_date'));
+        
+        //Time
+        add_shortcode('time',array($this,'get_time'));
+        add_shortcode('modified-time',array($this,'modified_time'));
+        
+        //Category Shortcodes
+        add_shortcode('category',array($this,'get_category'));
+        add_shortcode('tags',array($this,'get_tags'));
+        
+        //Author post link
+        add_shortcode('author-post-link',array($this,'author_post_link'));
+        
+        //Comment link
+        add_shortcode('comment-link',array($this,'comment_link'));
+        
 
     }
     /**
@@ -43,9 +59,147 @@ class FPLoopShortCodes {
 
         return self::$instance;
     }
+    
+    /**
+     * Get Date
+     * 
+     * [date],[date formate=""]
+     * 
+     * @param type $atts
+     * @return type
+     */
     public function get_date($atts){
-        return date('F jS, Y');
+        
+        extract(shortcode_atts(array(
+                    'formate' => '',
+                    'class'=>'date',
+                        ), $atts));
+        
+        return '<div  class="' . esc_attr($class) . '">' .'Date: &nbsp;'. get_the_date($formate). '</div>'; 
+       
     }
+    /**
+     * Get last modified date
+     * 
+     * [modified-date],[modified-date formate=""]
+     * @param type $atts
+     * @return type
+     */
+    public function modified_date($atts){
+        
+        extract(shortcode_atts(array(
+            'formate'=>'',
+            'class'=>'modi-date',
+        ),$atts));
+     
+       return '<div  class="' . esc_attr($class) . '">' .'Last Modified Date: &nbsp;'. get_the_modified_date($formate). '</div>';
+    }
+    /**
+     * get time
+     * 
+     * [time],[time formate=""]
+     * 
+     * @param type $atts
+     * @return type
+     */
+    public function get_time($atts){
+        
+        extract(shortcode_atts(array(
+                    'formate' => '',
+                    'class' => 'time',
+                        ), $atts));
+        
+          return '<div  class="' . esc_attr($class) . '">' .'Time: &nbsp;'. get_the_time($formate). '</div>'; 
+      
+    }
+    /**
+     * Get last modified time
+     * 
+     * [modified-time],[modified-time formate=""]
+     * 
+     * @param type $atts
+     * @return type
+     */
+    public function modified_time($atts){
+        
+        extract(shortcode_atts(array(
+                    'formate' => '',
+                    'class' => 'modi-time',
+                        ), $atts));
+        
+        return '<div  class="' . esc_attr($class) . '">' .'Last Modified Time: &nbsp;'. get_the_modified_time($formate). '</div>';
+       
+    }
+    /**
+     * Get Categorys
+     * 
+     * [category]
+     * 
+     * @param type $atts
+     * @param type $content
+     * @return type
+     */
+    public function get_category($atts,$content=null){
+        
+        extract(shortcode_atts(array(
+            
+                    'class' => 'category',
+                        ), $atts));
+
+        return '<div  class="' . esc_attr($class) . '">' . get_the_category_list(',' ). '</div>'; 
+	
+    }
+    /**
+     * Tags
+     * 
+     * [tags]
+     * 
+     * @param type $atts
+     * @param type $content
+     * @return type string
+     */
+    public function get_tags($atts,$content=null){
+        extract(shortcode_atts(array(
+                    'class' => 'tag-cloud',
+                        ), $atts));
+
+        return '<div  class="' . esc_attr($class) . '">'.'Tags: &nbsp;' . get_the_tag_list('', ',') . '</div>';
+    }
+    /**
+     * Author post link
+     * 
+     * [author-post-link]
+     * 
+     * @param type $atts
+     * @return type
+     */
+    public function author_post_link($atts){
+        
+        extract(shortcode_atts(array(
+                    'class' => 'author-link',
+                        ), $atts));
+
+        return '<div  class="' . esc_attr($class) . '">'.'Posted By: &nbsp;' . get_the_author_link() . '</div>';
+    }
+    /**
+     * Comment link
+     * 
+     * [comment-link]
+     * 
+     * @param type $atts
+     * @return type
+     */
+    public function comment_link($atts){
+        
+        extract(shortcode_atts(array(
+                    'class' => 'comment-link',
+                        ), $atts));
+
+        return '<div  class="' . esc_attr($class) . '">'.'Comment link: &nbsp;' . get_comments_link() . '</div>';
+        
+    }
+  
 }
+
 FPLoopShortCodes::get_instance();
 ?>
